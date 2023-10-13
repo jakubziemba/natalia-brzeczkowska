ecc87f3a60907b60f62261e93f5aa24a055b7854
Jakub Ziemba <ziemba.jak@gmail.com>
2023-10-13 20:25:34 +0200

    [FEAT] Basic config & homepage test

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      darkberry: '##43317F',
+      lightberry: '##9B8CBE',
+      'berry-white': '#F7F1FF',
+      red: '#C02400',
+      lightred: '#FFEDEC',
+      gray: '#333333',
+      'almost-white': '#FFFEFA',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        serif: ['var(--font-serif)'],
+        sans: ['var(--font-sans)'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
