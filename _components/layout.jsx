f0a86d60ca7012ad61fac350a7bdfe0bd5270755
Jakub Ziemba <ziemba.jak@gmail.com>
2024-05-09 17:42:21 +0200

    (fix) add new menu animation

--- _components/layout.jsx
+++ _components/layout.jsx
@@ -1,17 +1,18 @@
 import Nav from "./nav";
 import { motion } from "framer-motion";
-// import LenisScroller from "./LenisScroller";
 
 export default function Layout({ children, className = "" }) {
   return (
-    <motion.main
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { duration: 0.4 } }}
-      exit={{ opacity: 0 }}
-      className=""
-    >
+    <>
       <Nav />
-      {children}
-    </motion.main>
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.4 } }}
+        exit={{ opacity: 0 }}
+        className=""
+      >
+        {children}
+      </motion.main>
+    </>
   );
 }
--- _components/nav.jsx
+++ _components/nav.jsx
@@ -7,7 +7,7 @@ const links = [
   { id: 0, href: "/commercials", label: "Commercials" },
   { id: 1, href: "/music-videos", label: "Music Videos" },
   { id: 2, href: "/photoshoots", label: "Photoshoots" },
-  { id: 3, href: "/projects", label: "Projects" },
+  { id: 3, href: "/about", label: "About" },
   { id: 4, href: "/contact", label: "Contact" },
 ];
 
@@ -16,41 +16,46 @@ export default function Nav() {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
-    <nav className="sticky top-0 z-20 mx-auto grid w-full grid-cols-[1fr_auto] place-items-center gap-2 bg-transparent px-4 py-6 font-sans font-light [grid-area:nav] 2xl:mx-auto 2xl:max-w-screen-2xl 2xl:px-0">
-      <Link href="/" className="mr-auto w-max font-normal text-red">
+    <nav className="relative top-0 z-20 mx-auto flex w-full flex-row justify-between gap-2 bg-transparent px-4 py-6 font-sans font-light 2xl:mx-auto 2xl:max-w-screen-2xl 2xl:px-0">
+      <Link href="/" className="flex w-max font-normal text-red">
         Natalia Brzęczkowska
       </Link>
       <button className="rounded-full border border-red px-4 py-1.5 text-right text-red md:invisible md:hidden">
         Menu
       </button>
-      <ul className="invisible hidden flex-nowrap items-center text-sm md:visible md:flex">
+      <ul className="invisible hidden text-sm md:visible md:flex">
         {links.map((link, index) => {
           const isActive = categoryHovered === link.label;
           return (
             <li key={index} className="leading-none">
               <Link
                 href={link.href}
-                className="relative px-3 py-1.5 transition-colors duration-[150ms] hover:text-almost-white"
+                className="flex w-full flex-col overflow-hidden px-3 py-1.5"
                 onMouseEnter={() => setCategoryHovered(link.label)}
                 onMouseLeave={() => setCategoryHovered("")}
               >
-                {link.label}
-                <AnimatePresence>
-                  {isActive && (
-                    <motion.span
-                      layoutId="nav-pill"
-                      initial={false}
-                      animate={{ scale: [0.75, 1], opacity: 1 }}
-                      exit={{ scale: [1, 0.75], opacity: 0 }}
-                      transition={{
-                        type: "spring",
-                        bounce: 0.15,
-                        duration: 0.35,
-                      }}
-                      className="absolute inset-0 -z-10 rounded-full bg-red"
-                    />
-                  )}
-                </AnimatePresence>
+                <span className="relative flex overflow-hidden">
+                  <motion.span
+                    initial={{ y: 0 }}
+                    animate={{ y: isActive ? -15 : 0 }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className="relative"
+                  >
+                    {link.label}
+                  </motion.span>
+                  <motion.span
+                    initial={{ y: 15 }}
+                    animate={{ y: isActive ? 0 : 15 }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className="absolute"
+                  >
+                    {link.label}
+                  </motion.span>
+                </span>
               </Link>
             </li>
           );
