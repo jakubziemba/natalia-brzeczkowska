f2f5ba6ff900b7896fcb10dd6efb99f34bfff6f1
Jakub Ziemba <ziemba.jak@gmail.com>
2024-05-17 21:08:09 +0200

    (feat) add mobile menu

--- _components/nav.tsx
+++ _components/nav.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useMediaQuery } from "usehooks-ts";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const links = [
   { id: 0, href: "/commercials", label: "Commercials" },
@@ -12,16 +12,28 @@ const links = [
 ];
 
 export default function Nav() {
+  const [showMenu, setShowMenu] = useState(false);
   const [categoryHovered, setCategoryHovered] = useState("");
   const isMobile = useMediaQuery("(max-width: 768px)");
 
+  useEffect(() => {
+    if (showMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [showMenu]);
+
   return (
-    <nav className="relative top-0 z-20 mx-auto flex w-full flex-row items-center justify-between gap-2 bg-transparent px-4 py-6 font-sans font-light 2xl:mx-auto 2xl:max-w-screen-2xl 2xl:px-0">
+    <nav className="relative top-0 mx-auto flex w-full flex-row items-center justify-between gap-2 bg-transparent px-4 py-6 font-sans font-light 2xl:mx-auto 2xl:max-w-screen-2xl 2xl:px-0">
       <Link href="/" className="flex w-max font-normal text-red">
         Natalia Brzęczkowska
       </Link>
       {isMobile ? (
-        <button className="px-3 py-1.5 text-red md:invisible md:hidden">
+        <button
+          className="px-3 py-1.5 text-red md:invisible md:hidden"
+          onClick={() => setShowMenu(!showMenu)}
+        >
           Menu
         </button>
       ) : null}
@@ -69,6 +81,42 @@ export default function Nav() {
           );
         })}
       </ul>
+      <AnimatePresence mode="wait">
+        {showMenu ? (
+          <motion.div
+            initial={{ y: "-100%" }}
+            animate={{ y: "0%" }}
+            exit={{ y: "-100%" }}
+            transition={{
+              type: "spring",
+              bounce: 0,
+            }}
+            className="absolute inset-0 z-30 flex h-screen w-full flex-row justify-center gap-2 bg-transparent px-4 py-6 backdrop-blur-3xl"
+          >
+            <button
+              className="absolute right-4 top-6 z-30 size-12 px-3 py-1.5 text-lg text-red md:invisible md:hidden"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              X
+            </button>
+            <ul className="flex flex-col items-center justify-center gap-2 text-xl">
+              {links.map((link, index) => {
+                return (
+                  <li key={index} className="text-red">
+                    <Link
+                      href={link.href}
+                      className="flex w-full flex-col overflow-hidden px-3 py-2 font-serif text-4xl font-[400]"
+                      onClick={() => setShowMenu(!showMenu)}
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     </nav>
   );
 }
