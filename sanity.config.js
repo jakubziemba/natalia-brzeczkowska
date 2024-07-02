9f86b4d800086970848fccf7bf905a6ec497c338
Jakub Ziemba <ziemba.jak@gmail.com>
2024-07-02 18:32:45 +0200

    (feat) add plugins to sanity

--- sanity.config.js
+++ sanity.config.js
@@ -6,6 +6,8 @@ import { defineConfig } from "sanity";
 import { visionTool } from "@sanity/vision";
 import { structureTool } from "sanity/structure";
 import { orderableDocumentListDeskItem } from "@sanity/orderable-document-list";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
+import { media } from "sanity-plugin-media";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env";
@@ -51,6 +53,8 @@ export default defineConfig(
       // Vision is a tool that lets you query your content with GROQ in the studio
       // https://www.sanity.io/docs/the-vision-plugin
       visionTool({ defaultApiVersion: apiVersion }),
+      vercelDeployTool(),
+      media(),
     ],
   },
   // {
