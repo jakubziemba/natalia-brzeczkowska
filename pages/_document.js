89a9279cb355fac6b9c897146c820463940d80c3
Jakub Ziemba <ziemba.jak@gmail.com>
2024-07-02 16:58:41 +0200

    (fix) og image fix?

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:title" content="Nati Makeupik" key="title" />
+        <meta
+          property="og:description"
+          content="A Warsaw-based make up artist, working since 2019 in many industry sectors including TV, theatre, fashion and film."
+        />
+        <meta property="og:url" content="https://natimakeupik.com" />
+        <meta property="og:image" content="/og-image.png" />
+        <meta name="twitter:title" content="Nati Makeupik" key="title" />
+        <meta
+          name="twitter:description"
+          content="A Warsaw-based make up artist, working since 2019 in many industry sectors including TV, theatre, fashion and film."
+        />
+        <meta name="twitter:image" content="/twitter-image.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
