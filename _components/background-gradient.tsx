48b01bb1311d081a8032476bd6e57c4a9493bdaf
Jakub Ziemba <ziemba.jak@gmail.com>
2024-07-04 12:45:57 +0200

    (fix) gradient z-index

--- _components/background-gradient.tsx
+++ _components/background-gradient.tsx
@@ -1,7 +1,7 @@
 import { memo } from "react";
 
 function BackgroundGradient() {
-  return <div className="bg-gradient fixed inset-0"></div>;
+  return <div className="bg-gradient fixed inset-0 -z-10"></div>;
 }
 
 export default memo(BackgroundGradient);
